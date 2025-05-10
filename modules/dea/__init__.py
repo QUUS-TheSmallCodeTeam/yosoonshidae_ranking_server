c7930a65830a957268b344c3f89c3c1c394d618e
MinsooCha <m8chaa@gmail.com>
2025-05-10 15:39:17 +0900

    dea method

--- modules/dea/__init__.py
+++ modules/dea/__init__.py
@@ -4,3 +4,4 @@ DEA (Data Envelopment Analysis) implementation for mobile plan ranking.
 
 from .dea_scipy import run_scipy_dea
 from .dea_run import run_dea_analysis
+from ..dea import calculate_rankings_with_dea, calculate_rankings_with_ties
